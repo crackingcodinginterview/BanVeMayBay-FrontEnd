a33784dfbc35e948eeb975a323cbc79b15a35b69
vominhquoc <sliverwestlife4@gmail.com>
2016-10-19 12:40:31 +0700

    test

---
+++ src/app.js
@@ -0,0 +1,16 @@
+define(function(require){
+    var angular = require('angular');
+    var appConfig = require('./app.config');
+    var appModule = require('./app/modules/index');
+    var uiRouter = require('uiRouter');
+    var module = angular.module('app', [
+        'ui.router',
+
+        appConfig,
+
+        'app.base',
+        'app.home'
+    ]);
+
+    return module;
+});
---
+++ src/app/modules/index.js
@@ -0,0 +1,8 @@
+define(function(require){
+    'use strict';
+
+    return {
+        base: require('./base/base'),
+        home: require('./home/home')
+    };
+});
